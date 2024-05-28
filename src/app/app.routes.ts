ada1e8058ce6e6522ee2f605f3b7922c59ec9691
EarthWormGym <adam56.beresford@gmail.com>
2024-05-28 22:15:48 +0100

    Setup basic landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: PortfolioComponent }];
