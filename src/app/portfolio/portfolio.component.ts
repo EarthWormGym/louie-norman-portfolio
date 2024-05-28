86a7aaa8a8ad8c461954ab8a571032eb87050fad
EarthWormGym <adam56.beresford@gmail.com>
2024-05-28 22:30:55 +0100

    basic layout

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.css'
+  styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
 
