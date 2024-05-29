8807dbbdcfe7b98745fb21f40b8c76625e311a2f
EarthWormGym <adam56.beresford@gmail.com>
2024-05-29 20:54:39 +0100

    Layout improvements

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
-
+  projects = [
+    { name: 'From Stone to Stone', link: 'from-stone-to-stone' },
+    { name: 'Bangers', link: 'bangers' },
+    { name: 'Jimmy and Jill UnDance', link: 'jimmy-and-jill-undance' },
+    { name: 'Rio Ferdinand Foundation', link: 'rio-ferdinand-foundation' },
+    { name: 'Interface', link: 'interface' },
+  ];
 }
