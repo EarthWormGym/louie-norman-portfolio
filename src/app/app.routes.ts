800850f9a8ad9df512963255518bfd1e9d3083fa
EarthWormGym <adam56.beresford@gmail.com>
2024-06-13 19:37:03 +0100

    Refactoring of routing and additions to about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { LouieNormanStudioComponent } from './portfolio/louie-norman-studio/louie-norman-studio.component';
 import { AboutComponent } from './portfolio/about/about.component';
 import { ContactComponent } from './portfolio/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: PortfolioComponent },
-    { path: 'louienormanstudio', component: LouieNormanStudioComponent },
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent }];
--- src/app/portfolio/about/about.component.ts
+++ src/app/portfolio/about/about.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  aboutItems = [
+    'Education',
+    'Kingston School of Art',
+    'Foundation Diploma (2018-19) - Distinction',
+    'Graphic design (BA) (2019-2023) - 1st Class',
+    'Skills',
+    'Camera Operator',
+    'Adobe InDesign',
+    'Adobe Photoshop',
+    'Adobe Permier Pro',
+    'Adobe After Effects',
+    'Adobe Lightroom/Classic',
+    'DaVinci Resolve'
+  ];
 }
